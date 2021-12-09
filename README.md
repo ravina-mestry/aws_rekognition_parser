9e2571e734f15d547cc6677c0199d465c449fe74
ravina-mestry <x21120501@student.ncirl.ie>
2021-12-09 23:29:38 +0000

    Added receipt text parser for AWS Rekognition response

---
+++ README.md
@@ -0,0 +1,4 @@
+# AWS Rekognition Parser Package
+
+This is a simple AWS Rekognition output parser.
+Currently it supports basic parsing for receipt text.
---
+++ src/aws_rekognition_parser_pkg/aws_rekognition_parser.py
@@ -0,0 +1,35 @@
+def parse_rekognition_response_receipt_text(rekognitionResponse, vendorNameList):
+    
+    receiptText = dict()
+
+    isVendorName = False
+    numTotal = 0
+    isAmountTotal = False
+
+    for text in rekognitionResponse['TextDetections']:
+        print( text['Type'] + ' ' + text['DetectedText'].lower() + ' : ' + str(text['Confidence']))
+
+        if numTotal == 1:
+            numTotal = 2
+        
+        if not isVendorName:
+            for vendorName in vendorNameList:
+                if text['DetectedText'].lower() == vendorName:
+                    receiptText['vendorName'] = vendorName
+                    isVendorName = True
+
+        if numTotal == 0:
+            if text['DetectedText'].lower() == 'total':
+                numTotal = 1
+        
+        if numTotal == 2:
+            numTotal = 0
+            amountTotal = text['DetectedText'].lower().replace('eur', '')
+            if amountTotal.replace('.', '').isnumeric():
+                receiptText['amountTotal'] = float(amountTotal)
+                isAmountTotal = True
+        
+        if isVendorName and isAmountTotal:
+            break
+
+    return receiptText
